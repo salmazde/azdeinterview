6f9de8d63d8de01a2681b6dd1868caa065f617af
salmazde <salmankhanazde@gmail.com>
2026-07-11 15:27:07 +0530

    Add service worker for caching and offline support

---
+++ service-worker.js
@@ -0,0 +1,101 @@
+const CACHE_NAME = "azde-prep-v1.0.0";
+
+const STATIC_FILES = [
+
+    "./",
+    "./index.html",
+    "./databricks.html",
+    "./databricks_old.html",
+    "./old.html",
+
+    "./manifest.json",
+
+    "./icon-192.png",
+    "./icon-512.png"
+
+];
+
+
+// INSTALL
+
+self.addEventListener("install", event => {
+
+    self.skipWaiting();
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+            .then(cache => cache.addAll(STATIC_FILES))
+
+    );
+
+});
+
+
+// ACTIVATE
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys =>
+
+            Promise.all(
+
+                keys.map(key => {
+
+                    if (key !== CACHE_NAME)
+
+                        return caches.delete(key);
+
+                })
+
+            )
+
+        )
+
+    );
+
+    self.clients.claim();
+
+});
+
+
+// FETCH
+
+self.addEventListener("fetch", event => {
+
+    if (event.request.method !== "GET") return;
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+            .then(cacheResponse => {
+
+                if (cacheResponse)
+
+                    return cacheResponse;
+
+                return fetch(event.request)
+
+                    .then(networkResponse => {
+
+                        const clone = networkResponse.clone();
+
+                        caches.open(CACHE_NAME)
+
+                            .then(cache => cache.put(event.request, clone));
+
+                        return networkResponse;
+
+                    })
+
+                    .catch(() => caches.match("./index.html"));
+
+            })
+
+    );
+
+});
